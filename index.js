8ab3452e4d31c90ee35ebab8b7e3164b2bfb7078
tjbulick <e.pichtchalnikov@gmail.com>
2019-07-21 22:15:41 +0300

    added  hander for click on buttons

---
+++ index.js
@@ -0,0 +1,14 @@
+const accordion = document.getElementsByClassName('accordion');
+
+for (let i = 0; i < accordion.length; i++) {
+	accordion[i].addEventListener('click', function() {
+		this.classList.toggle('active');
+
+		const panel = this.nextElementSibling;
+		if (panel.style.display === 'block') {
+			panel.style.display = 'none';
+		} else {
+			panel.style.display = 'block';
+		}
+	});
+}
